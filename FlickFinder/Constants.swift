55549c8a4aed7dbcbc5de54c222a7a369ca99928
QueeniePeng <pengqueenie@gmail.com>
2016-10-18 16:18:48 -0500

    Delete API key

--- FlickFinder/Constants.swift
+++ FlickFinder/Constants.swift
@@ -41,7 +41,7 @@ struct Constants {
     // MARK: Flickr Parameter Values
     struct FlickrParameterValues {
         static let SearchMethod = "flickr.photos.search"
-        static let APIKey = "f1677c470828e5e89e15d28bdfc2af06"
+        static let APIKey = "YOUR_API_KEY_HERE"
         static let ResponseFormat = "json"
         static let DisableJSONCallback = "1" /* 1 means "yes" */
         static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
